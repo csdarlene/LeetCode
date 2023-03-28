15a3821ac71c5146a718be7cfe7956dff8dd8b35
Darlene <73172541+csdarlene@users.noreply.github.com>
2023-03-28 11:25:46 -0300

    Commit

--- src/code/learn/TwoSums/TwoSums.java
+++ src/code/learn/TwoSums/TwoSums.java
@@ -1,2 +1,74 @@
-package code.learn.TwoSums;public class TwoSums {
+package code.learn.TwoSums;
+
+import java.util.Arrays;
+
+public class TwoSums {
+    public static String twoSum(int[] nums, int target) {
+        int size = nums.length;
+        int[] result = new int[2];
+        int i = 0;
+        while (i < size) {
+            for (int a : nums) {
+                if (nums[i] == a) {
+                    result[0] = i;
+                    i++;
+                    for (int b : nums) {
+                        if (nums[i] == b) {
+                            result[1] = i;
+                            if (a + b == target) {
+                                if (result[0] != result[1]) {
+                                    return Arrays.toString(result);
+                                }
+                            }
+                        }
+
+                    }
+                }
+
+            }
+        }
+        return Arrays.toString(result);
+    }
+
+
+    public static String two(int[] nums, int target) {
+        int size = nums.length;
+        int[] result = new int[2];
+        int i = 0;
+        while (i < size) {
+            for (int j = i + 1; j < size; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    return Arrays.toString(result);
+                }
+            }
+            i++;
+        }
+        return Arrays.toString(result);
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(twoSum(new int[]{3, 2, 4}, 6));
+        System.out.println(Sum(new int[]{2, 7, 11, 15}, 9));
+        System.out.println(two(new int[]{3, 2, 3, 5}, 6));
+    }
+
+    public static String Sum(int[] nums, int target) {
+        int size = nums.length;
+        int[] result = new int[2];
+        int i = 0;
+        while (i < size) {
+            for (int j = i + 1; j < size; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    return Arrays.toString(result);
+                }
+            }
+        }
+        return Arrays.toString(result);
+    }
+
 }
--- src/code/learn/TwoSums/TwoSumsTest.java
+++ src/code/learn/TwoSums/TwoSumsTest.java
@@ -1,4 +1,28 @@
+package code.learn.TwoSums;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class TwoSumsTest {
-  
+TwoSums sums= new TwoSums();
+    @org.junit.jupiter.api.Test
+    public void twoSum() {
+        assertEquals("[1, 2]", sums.twoSum(new int[]{3, 2, 4}, 6));
+        assertEquals("[0, 1]", sums.twoSum(new int[]{2, 7, 11, 15}, 9));
+    }
+
+    @org.junit.jupiter.api.Test
+    void two() {
+        assertEquals("[1, 2]", sums.Sum(new int[]{3, 2, 4}, 6));
+        assertEquals("[0, 1]", sums.Sum(new int[]{2, 7, 11, 15}, 9));
+        assertEquals("[0, 2]", sums.Sum(new int[]{3, 2, 3, 5}, 6));
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void sum() {
+        assertEquals("[1, 2]", sums.two(new int[]{3, 2, 4}, 6));
+        assertEquals("[0, 1]", sums.two(new int[]{2, 7, 11, 15}, 9));
+        assertEquals("[0, 2]", sums.two(new int[]{3, 2, 3, 5}, 6));
+
+    }
 }
